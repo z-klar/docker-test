2d56fa717ab89968ff691cc33505334063802db2
Zdenek Klar <zdenek.klar@gmail.com>
2020-11-06 13:04:17 +0100

    Tests for injecting config from outside

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 echo "Starting docker-test ... "
 
-sed -ire "s~params.param1=Parameter1~params.param1=${PARAMETER1}~" /var/application.properties
-sed -ire "s~params.param2=Parameter2~params.param2=${PARAMETER2}~" /var/application.properties
+#sed -ire "s~params.param1=Parameter1~params.param1=${PARAMETER1}~" /var/application.properties
+#sed -ire "s~params.param2=Parameter2~params.param2=${PARAMETER2}~" /var/application.properties
 
-echo "= TEST config =================================================="
-cat /var/application.properties
-echo "= End of TEST config ==========================================="
+#echo "= TEST config =================================================="
+#cat /var/application.properties
+#echo "= End of TEST config ==========================================="
 
-java -jar /var/app.jar --spring.config.location=/var/application.properties
+#java -jar /var/app.jar --spring.config.location=/var/application.properties
 
+java -jar /var/app.jar --spring.config.location=/configdst/application.properties
